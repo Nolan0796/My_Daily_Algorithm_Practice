6a369107db79f33cf47f248f6ce2d1acbd87b43a
Nolan Hill <86444916+Nolan0796@users.noreply.github.com>
2022-02-24 13:43:00 -0700

    Add files via upload
    print1toNum Algorithm

---
+++ 2-24-22.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Write a program that would print all the numbers from 1 to 255
+
+def print1toNum(num):
+    for x in range(1,num+1):
+        print(x)
+
+print1toNum(255)
