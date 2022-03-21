26c724127b8669a5dee90f3829c5f8669eabb2b0
Nolan Hill <nolanjhill@yahoo.com>
2022-03-21 12:41:53 -0700

    removingNegatives only using pop() method.

--- 3-21-22.js
+++ 3-21-22.js
@@ -1,13 +1,28 @@
 // Given an array of multiple values (e.g. [0, -1, 2, -3, 4, -5, 6]), write a program that removes any negative values in that array.  Once your program is done, the array should be composed of only the non-negative numbers, in their original order.  Do this without creating a temporary array.
 
 function removingNegatives(arr) {
-    for(i=0; i<arr.length; i++) {
-        if(arr[i] < 0) {
-            arr.splice([i], 1)
+    var count = 0
+    for(var e=0; e<arr.length; e++) {
+        if(arr[e] < 0) {
+            count += 1
         }
+    }
+    for(var i=0; i<arr.length; i++) {
+        if(arr[i] < 0) {
+            for(var y=i+1; y < arr.length; y++)
+                if(arr[y] > -1) {
+                    arr[i] = arr[y]
+                    if(y < arr.length-1){
+                        arr[y] = arr[y+1]
+                    }
+                    break
+                }
+        }
+    }
+    for(var x=0 ; x < count; x++) {
+        arr.pop()
     }
     return arr
 }
 
 console.log(removingNegatives([0, -1, 2, -3, 4, -5, 6]))
-
