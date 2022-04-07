b8daba34cc583245bd2f6f3881edbbd623275fee
Nolan Hill <nolanjhill@yahoo.com>
2022-04-07 16:36:23 -0700

    divisibleSumPairs algorithm.

---
+++ 4-7-22.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'divisibleSumPairs' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#  3. INTEGER_ARRAY ar
+#
+
+def divisibleSumPairs(n, k, ar):
+    pairs = 0
+    for i in range(n):
+        for j in range(i+1, n):
+            if (ar[i] + ar[j]) % k == 0:
+                pairs += 1
+    return pairs
+
+print(divisibleSumPairs(6, 3, [1, 3, 2, 6, 1, 2]))
